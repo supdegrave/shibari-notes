e4879b2b4a7040af130aaf527e803bcbf9950f05
Stuart Updegrave <stuart@updegrave.com>
2018-12-30 14:11:57 +0100

    ng add @angular/material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeViewComponent } from './views/home-view/home-view.component';
 import { LoginViewComponent } from './views/login-view/login-view.component';
 import { TieViewComponent } from './views/tie-view/tie-view.component';
 import { TiesViewComponent } from './views/ties-view/ties-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -20,7 +21,8 @@ import { TiesViewComponent } from './views/ties-view/ties-view.component';
     imports: [
         BrowserModule,
         RoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        BrowserAnimationsModule
     ],
     providers: [],
     bootstrap: [
