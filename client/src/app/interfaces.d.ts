8da39c32a7d76ec383ae867d03e3d85e2e74525e
Stuart Updegrave <stuart@updegrave.com>
2018-12-30 12:38:26 +0100

    add interface namespace

---
+++ client/src/app/interfaces.d.ts
@@ -0,0 +1,11 @@
+declare namespace ShibariNotes {
+    interface Tie {
+        _id: string;
+        name: string;
+        teacher: String;
+        style: String;
+        learningContext: String;
+        tags: string[];
+        created: string;
+    }
+}
